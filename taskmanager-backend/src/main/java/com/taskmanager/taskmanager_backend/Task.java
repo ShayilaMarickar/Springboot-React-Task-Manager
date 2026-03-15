b94b19e381444e9a46602cb93ea73d60783d84b6
ShayilaMarickar <marickarshayila@gmail.com>
2026-03-16 01:07:23 +0530

    Initial commit: Uploading Fullstack Task Manager - SpringBoot + React

---
+++ taskmanager-backend/src/main/java/com/taskmanager/taskmanager_backend/Task.java
@@ -0,0 +1,16 @@
+package com.taskmanager.taskmanager_backend;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private boolean completed = false;
+}
---
+++ taskmanager-backend/src/main/java/com/taskmanager/taskmanager_backend/TaskController.java
@@ -0,0 +1,36 @@
+package com.taskmanager.taskmanager_backend;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "http://localhost:3000")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task toggleTask(@PathVariable Long id) {
+        Task task = taskRepository.findById(id).orElseThrow();
+        task.setCompleted(!task.isCompleted());
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+}
